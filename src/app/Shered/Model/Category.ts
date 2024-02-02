0876ad7ef0665f2bdd360bdb3b2af6db6e869c8d
lirontram <liron0377@gmail.com>
2024-02-02 20:06:01 +0200

    we

--- src/app/Shered/Model/Category.ts
+++ src/app/Shered/Model/Category.ts
@@ -21,12 +21,14 @@ export class Category {
     origin : String;
     target : string;
     lastUpdateDate = new Date();
+    
     words : TranslatedWord[] = [];
     
     constructor(id: number,
     name : string,
     origin : string,
-    target : string) {
+    target : string,
+    ) {
     this.id = id;
     this.name = name;
     this.origin = origin;
